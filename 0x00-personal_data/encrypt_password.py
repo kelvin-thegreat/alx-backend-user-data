565d28f988dddc7300f65318394d345a3d324b42
SE. kelvin <71261868+kelvin-thegreat@users.noreply.github.com>
2023-11-02 17:06:05 +0300

    Encrypting and Checking Password Validity

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -16,3 +16,17 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """function that expects 2 arguments and returns a boolean.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+"""   
+def hash_password(password):
+    ''' Generate a random salt '''
+    salt = bcrypt.gensalt()
+    
+    ''' Hash the password using the generated salt '''
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
+
+def is_valid(hashed_password, password):
+    ''' Verify if the provided password matches the hashed password '''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+"""
