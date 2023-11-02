8b9f2c589216fe9acb6c59bc0f194344ec5ab748
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-02 06:21:03 -0700

    Task 0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,11 +15,8 @@ patterns = {
 }
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str,
-        ) -> str:
-    """Filters a log line.
-    """
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str,) -> str:
+    """Filters a log line."""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
 
